2820f0a4fe546b5ffe2e2da8fb9107c7fc010c8c
Ruchira.Weerakkody <Ruchira.Weerakkody@LKCOL-PW03X0GK.peaktrav.com>
2024-01-02 16:22:29 +0530

    Demo test suit

---
+++ Test Suites/Demo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1effa382-1013-4ca4-b296-0215f0ecaee9</testSuiteGuid>
+   <testCaseLink>
+      <guid>8412ed5e-fe9f-4e6e-8fe1-601f28a25d29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POC/TestDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
